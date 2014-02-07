2652658b62f482e04a4213f6f31bbf7c97e2409e
Howard Tang <tangh@google.com>
2014-02-07 10:23:02 +0000

    Add app that was deployed.

---
+++ ex/ex.js
@@ -0,0 +1,35 @@
+
+Alerts = new Meteor.Collection("alerts")
+
+if (Meteor.isClient) {
+
+    Alerts.find().observe({
+      added: function(item) {
+
+        var el   = document.querySelector('.fill');
+        var word = el.querySelector('.word');
+
+        // Update color and text.
+        el.style.background = item.color;
+        word.innerHTML      = item.word;
+
+        // Center the word element.
+        word.style.marginLeft = '-' + word.offsetWidth / 2 + 'px';
+        word.style.marginTop  = '-' + word.offsetHeight / 2 + 'px';
+
+      }
+    });
+
+}
+
+if (Meteor.isServer) {
+	Meteor.methods({
+    'cλz_-=@z@`€£$%&*)_+_++±_-`': function(a, b) {
+      var a = a || 'No params passed yet :(';
+      var b = b || 'yellow';
+      Alerts.insert({word: a, color: b});
+    }
+  });
+
+  Alerts.remove({});
+}
